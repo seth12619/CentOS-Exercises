b82bdb696d6a96850f4039bde21a49b37ee317a3
Seth <seth_12619@yahoo.com.ph>
2017-06-28 15:44:41 +0800

    Minimum - For memory_check

--- memory_check.sh
+++ memory_check.sh
@@ -5,28 +5,29 @@ calc(){
 	}
 
 total_mem=$(free -m | grep Mem: | awk '{ print $2}') #Total Memory
-echo $total_mem
+
 used_mem=$(free -m | grep Mem: | awk '{ print $3}')
-perc_used= calc "($used_mem/$total_mem)*100"
-echo $perc_used
+perc_used=$(echo "scale=2 ; $used_mem / $total_mem" | bc)
+perc_used=$(echo "scale=2 ; $perc_used * 100" | bc)
+
 
 usage() {
 	echo "Usage: $0 [-c int] [-w int] [-e email]" 1>&2; exit 1;
 }
+
 while :; do
 while getopts ":c:w:e:" o; do
     case "${o}" in
         c)
             c=${OPTARG}
-			echo $c
             ;;
         w)
             w=${OPTARG}
-			echo $w
+			
             ;;
 		e)	e=${OPTARG}
 			#mail -s "Test" $e
-			echo $e
+			
 			;;
         *)
             usage
@@ -36,17 +37,21 @@ while getopts ":c:w:e:" o; do
 done
 ((OPTIND++))
 [ $OPTIND -gt 3 ] && break
+
 done
 
-if [ "$c" < "$w" ]
+percRound=${perc_used%%.*} 
+
+
+if [ "$c" -lt "$w" ]
 then
 	echo "Invalid Inputs - c must be greater than w"
 	exit 1
-elif [  "$perc_used%%." -ge "$c" ]
+elif [  "$percRound" -ge "$c" ]
 then
 	echo "Critical Error - Sending Mail"
 	exit 2
-elif [ "$perc_used%%." -ge "$w" ]
+elif [ "$percRound" -ge "$w" ]
 then
 	echo "Warning"
 	exit 1
