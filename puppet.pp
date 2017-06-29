9fa666f5596be1bb348fa10105feeadba07b0aa7
Seth <seth_12619@yahoo.com.ph>
2017-06-29 15:25:39 +0800

    Puppet - User

--- puppet.pp
+++ puppet.pp
@@ -1,10 +1,14 @@
 class puppet {
 	package { 
-		ensure => 'installed'
+		ensure	=>	'installed'
 	}
 	package { 'vim': }
 	package { 'curl': }
 	package { 'git': }
 	
-	
+	user { 'monitor':
+		ensure	=>	'present',
+		home	=>	'/home/monitor',
+		shell	=>	'/bin/bash',
+	}
 }
